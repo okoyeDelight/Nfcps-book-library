6ed0a6235798c8a2aae15132713b5ae6a9f2b8d4
Okoye Chiagozie Delight <chinedubisiola04@gmail.com>
2026-09-09 22:27:02 +0100

    Load immersive NFCPS design layer

--- appdeploy-live/pages/_app.tsx
+++ appdeploy-live/pages/_app.tsx
@@ -11,6 +11,7 @@ import '../styles/promo.css';
 import '../styles/watch.css';
 import '../styles/app-shell.css';
 import '../styles/product-v5.css';
+import '../styles/immersive-v6.css';
 
 export default function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
