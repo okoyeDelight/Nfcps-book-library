f650863d43863d8446f22ebbd820cadabe88746d
Okoye Chiagozie Delight <chinedubisiola04@gmail.com>
2026-09-10 23:15:22 +0100

    Load Apple-style UI typography

--- appdeploy-live/pages/_app.tsx
+++ appdeploy-live/pages/_app.tsx
@@ -19,6 +19,7 @@ import '../styles/concept-v3.css';
 import '../styles/account-sync.css';
 import '../styles/account-board.css';
 import '../styles/books-v49.css';
+import '../styles/apple-ui.css';
 import {NfcpsAccountProvider} from '../src/AccountSync';
 
 export default function App({ Component, pageProps }: AppProps) {
