cca045677a6774b03c1b6242191de5bb4ad95c3f
Okoye Chiagozie Delight <chinedubisiola04@gmail.com>
2026-09-10 10:44:10 +0100

    Load v47 concept design system

--- appdeploy-live/pages/_app.tsx
+++ appdeploy-live/pages/_app.tsx
@@ -15,6 +15,7 @@ import '../styles/immersive-v6.css';
 import '../styles/living-sanctuary-v2.css';
 import '../styles/ui-v2.css';
 import '../styles/v45-fixes.css';
+import '../styles/concept-v3.css';
 
 export default function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
