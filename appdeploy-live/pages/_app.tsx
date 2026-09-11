9155b1aa62c4ebc4f5cf9d5b1005aba6245a786d
Okoye Chiagozie Delight <chinedubisiola04@gmail.com>
2026-09-11 15:13:51 +0100

    Import Spatial Home production stylesheet in GitHub mirror

--- appdeploy-live/pages/_app.tsx
+++ appdeploy-live/pages/_app.tsx
@@ -22,6 +22,7 @@ import '../styles/books-v49.css';
 import '../styles/apple-ui.css';
 import '../styles/reader-flip.css';
 import '../styles/navigation-safety.css';
+import '../styles/spatial-home.css';
 import {NfcpsAccountProvider} from '../src/AccountSync';
 
 export default function App({ Component, pageProps }: AppProps) {
