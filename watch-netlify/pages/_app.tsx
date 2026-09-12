cf6960543d0b8f189059babd2cca29173ccae242
Okoye Chiagozie Delight <chinedubisiola04@gmail.com>
2026-09-12 18:58:51 +0100

    Load focused Watch styling

--- watch-netlify/pages/_app.tsx
+++ watch-netlify/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import '../styles/frontdoor.css';
+import '../styles/watch-cleanup.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
